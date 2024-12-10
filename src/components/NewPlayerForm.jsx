d8f54eab140b31572def68fd1fe1d039d9b7827b
Jackson G <jagman00@gmail.com>
2024-12-10 12:29:16 -0500

    new repo commit

---
+++ src/components/NewPlayerForm.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import { addPlayer } from "../API";
+
+const NewPlayerForm = ({setFetchToggle}) => {
+    const [name, setName] = useState("");
+    const [breed, setBreed] = useState(""); 
+    const [imageUrl, setImageUrl] = useState("");
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+
+        const result = await addPlayer(name,breed);
+        
+        if (result && result.success) {
+            setName("");
+            setBreed("");
+            setFetchToggle((prev)=>!prev);
+        } else {
+            console.error("Failed to add player!");
+        }
+    }
+
+    return ( 
+    <form onSubmit={handleSubmit}>
+        <label>
+            Name: {" "}
+            <input type="text" value={name} onChange={(e)=>setName(e.target.value)}/>
+        </label>
+        <label>
+            Breed: {" "}
+            <input type="text" value={breed} onChange={(e)=>setBreed(e.target.value)}/>
+        </label>
+        <label>
+            Image Url: {" "}
+            <input type="text" value={imageUrl} onChange={(e)=>setImageUrl(e.target.value)}/>
+        </label>
+        <button type="submit">Submit</button>
+    </form>
+    );
+}
+ 
+export default NewPlayerForm;
